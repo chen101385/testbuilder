f8b6ba01d94da0840f291876cfd186a62b6c43d8
Chris <chen1385@gmail.com>
2017-10-24 12:38:57 -0700

    completed Testbuilder Exercise

--- detectNetwork.js
+++ detectNetwork.js
@@ -80,10 +80,3 @@ Maestro
 */
 
 
-
-
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
